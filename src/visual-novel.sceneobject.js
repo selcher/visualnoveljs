949b5599d9d68c107608714036eaec8f779a541d
selcher <selcher123@gmail.com>
2015-07-30 19:40:50 +0800

    Update scene object module

--- src/visual-novel.sceneobject.js
+++ src/visual-novel.sceneobject.js
@@ -15,7 +15,9 @@
 					{ x : self.sceneFloorWidth, y : -self.sceneFloorHeight, z : self.sceneFloorHeight }
 				);
 
-			var sceneObject = self.objectFactory( "SceneObject", width, height, pos );
+			var sceneObject = getSceneObjectConstructor( self );
+
+			sceneObject = sceneObject( width, height, pos );
 
 			sceneObject.setBackground( width, height, 
 				bgInfo.path ? self.imgPath + bgInfo.path : "", bgInfo.color );
@@ -200,6 +202,138 @@
 		}
 
 		this.eventTracker.addEvent( "nowait", eventToAdd );
+
+	};
+
+
+
+
+
+
+	var getSceneObjectConstructor = function( novel ) {
+
+		var Sprite = novel.sprite;
+
+		/**
+		 *	Object : Scene
+		 *  States:
+		 *		1. sprite
+		 *  Behaviors:
+		 *		1. Move
+		 *		2. Rotate
+		 *		3. FadeIn/FadeOut
+		 **/
+		function SceneObject( width, height, position, transformOrigin, rotate ) {
+
+			if ( this instanceof SceneObject ) {
+
+				// Call the parent constructor
+				Sprite.apply( this, arguments );
+
+				// Add states
+
+				this.init( width, height, position, transformOrigin, rotate );
+
+				return this;
+
+			} else {
+
+				return new SceneObject( width, height, position, transformOrigin, rotate );
+
+			}
+
+		}
+
+		// Create a SceneObject.prototype object that inherits from Sprite.prototype
+		SceneObject.prototype = Object.create( Sprite.prototype );
+
+		// Set the "constructor" property to refer to SceneObject
+		SceneObject.prototype.constructor = SceneObject;
+
+		SceneObject.prototype.init = function init( width, height, position, transformOrigin, rotate ) {
+
+			var pos = {
+				x : position.x - 25,
+				y : position.z,
+				z : position.y
+			};
+			var trans = {
+				x : position.x + ( width / 2 ),
+				y : 0
+			};
+
+			var sceneObjectContainer = this.createSprite( 50, 50, pos, trans );
+
+			sceneObjectContainer.addClassName( "sceneObjectContainer" );
+			
+			pos = {
+				x : -( width / 2 ) + 25,
+				y : -height,
+				z : 0
+			};
+			trans = {
+				x : 25,
+				y : 0,
+				z : 0
+			};
+			var rot = {
+				x : 90, //+ 20, // refactor
+				y : 0,
+				z : 0
+			};
+
+			var sceneObject = this.createSprite( width, height, pos, trans, rot );
+			sceneObject.addClassName( "sceneObject" ).update();
+
+			// Add scene object ( perpendicular to floor )
+			// inside container ( parallel to floor )
+			// Note: to move object => move container
+			//       to rotate object => rotate object
+			sceneObjectContainer.addChild( sceneObject );
+
+			this.sprite = sceneObjectContainer;
+
+		};
+
+		SceneObject.prototype.setBackground = function setBackground( width, height, image, color ) {
+
+			// this.sprite => sceneObjectContainer
+			var sceneObject = this.sprite.children[ 0 ];
+
+			var bgSize = width && height ? "background-size:" + width + "px " + height + "px;" : "";
+			var bgImage = image ? "background-image:url('" + image + "');" : "";
+			var bgColor = color ? "background-color:" + color + ";" : "";
+			var preSemicolon = bgSize || bgImage || bgColor ? ";" : "";
+
+			sceneObject.style.cssText += preSemicolon + bgSize + bgImage + bgColor;
+
+		};
+
+		SceneObject.prototype.rotate = function rotate( axis, angle, loop, sprite ) {
+
+			// this.sprite => sceneObjectContainer
+			var sceneObject = this.sprite.children[ 0 ];
+
+			Sprite.prototype.rotate.call( this, axis, angle, loop, sceneObject );
+
+		};
+
+		SceneObject.prototype.rotateTo = function rotateTo( axis, angle, duration, sprite ) {
+
+			// this.sprite => sceneObjectContainer
+			var sceneObject = this.sprite.children[ 0 ];
+
+			Sprite.prototype.rotateTo.call( this, axis, angle, duration, sceneObject );
+
+		};
+
+		getSceneObjectConstructor = function() {
+
+			return SceneObject;
+
+		};
+
+		return SceneObject;
 
 	};
 
