6a54bbd787923253b3be22f82c73d875a1c13545
selcher <selcher123@gmail.com>
2015-08-30 12:45:57 +0800

    Moved code to choice dialog module

--- src/visual-novel.dialog.js
+++ src/visual-novel.dialog.js
@@ -1,8 +1,5 @@
 ( function( VN ) {
 
-	// TODO: separate dialog and menu
-	//		 create module for menu
-
 	// Use in config
 	VN.prototype.setDialogBgImage = function setDialogBgImage( img, width, height ) {
 
@@ -290,153 +287,6 @@
 
 
 
-	VN.prototype.choice = function choice( choiceEventId, listOfChoices, menuPos, menuImg ) {
-
-		// choiceEventId = to track the choice event
-
-		var self = this;
-		var totalChoices = listOfChoices.length;
-
-		// For choice menu:
-		// 1. build choice buttons
-		// 2. build choice image
-		// 3. insert buttons and image to menu container
-		var choicesTemplate = this.buildMenuChoices( listOfChoices, menuImg );
-
-		//  choice menu position
-		var pos = this.util.scalePosition(
-					{ "x": menuPos.x, "y": menuPos.y },
-					{ "x": this.screenWidth, "y": this.screenHeight }
-				);
-
-		function eventToAdd() {
-
-			// show
-			self.dialog.setMenuChoicesDisplay( true );
-
-			// init event to menu choices 
-			// and store the choices 
-			// to perform the choices on button click
-
-			// Store choices to call later on button click
-			self.dialog.menuChoices[ choiceEventId ] = listOfChoices.slice();
-
-			self.dialog.setMenuChoices( choicesTemplate );
-
-			self.dialog.updateMenuChoicesReference();
-
-			// Attach click events to dialog menu choice buttons
-			self.dialog.addMenuChoicesHandler(
-				choiceEventId,
-				listOfChoices,
-				function() {
-
-					// add event to reset menu choices so
-					// that it only gets called after
-					// all menu choice actions are done
-					self.resetMenuChoicesForEvent( choiceEventId );
-
-					// start events in menu choice action
-					self.eventTracker.startEvent(); 
-				}
-			);
-
-			// position choice menu
-			if ( menuPos ) {
-
-				self.dialog.setMenuChoicesPosition( pos.x, pos.y );
-
-			}
-
-			// add new event
-			self.eventTracker.addNewEventInProgress( choiceEventId );
-
-			// Debug
-			// console.log( "choice: " + choiceEventId );
-			// console.log( self.eventTracker.eventsInProgress );
-			// console.log( self.eventTracker.eventList );
-			// console.log( self.eventTracker.eventId );
-			
-		}
-
-		this.eventTracker.addEvent( "wait", eventToAdd );
-
-	};
-
-	VN.prototype.buildMenuChoices = function buildMenuChoices( listOfChoices, menuImage ) {
-
-		// Get menu choices template
-		var menuImg = menuImage ? menuImage : { "image": "", "width": 0, "height": 0 };
-		var imgPath = menuImage ? this.imgPath + menuImg.image : "";
-		var menuChoiceTemplate = this.getMenuChoicesTemplate( listOfChoices, imgPath, 
-			menuImg.width, menuImg.height );
-
-		return menuChoiceTemplate;
-
-	};
-
-	/**
-	 * Variable: menuChoiceTemplate
-	 *
-	 * Template for menu choice.
-	 */
-	VN.prototype.menuChoiceTemplate = [
-		"<div id='{novelId}-dialogMenuChoiceContainer' class='dialogMenuChoiceContainer'>",
-			"<div id='{novelId}-dialogMenuChoiceButtonsContainer' class='dialogMenuChoiceButtonsContainer'>",
-				"<foreach={choice in choices}>",
-					"<button class='dialogMenuChoiceButton' id='{novelId}-dialogMenuChoiceButton{index}' >",
-						"{choice.label}",
-					"</button><br/>",
-				"</foreach>",
-			"</div>",
-			"<div id='{novelId}-dialogMenuChoiceImageContainer' class='dialogMenuChoiceImageContainer'>",
-				"<if={imgPath}><img src='{imgPath}' style='width:{imgWidth}px;height:{imgHeight}px;' /></if>",
-			"</div>",
-		"</div>"
-	].join( "" );
-
-	VN.prototype.getMenuChoicesTemplate = function getMenuChoicesTemplate( choices, imgPath, imgWidth, imgHeight ) {
-
-		// variables to replace in template
-		var toReplace = {
-			"novelId": this.novelId,
-			"choices": choices,
-			"imgPath": imgPath,
-			"imgWidth": imgWidth,
-			"imgHeight": imgHeight
-		};
-
-		// get template
-		var menuChoiceTemplate = this.parser.parseTemplate( this.menuChoiceTemplate, toReplace );
-
-		return menuChoiceTemplate;
-
-	};
-
-	VN.prototype.resetMenuChoicesForEvent = function resetMenuChoicesForEvent( choiceEventId ) {
-
-		var self = this;
-
-		function eventToAdd() {
-			
-			self.dialog.resetMenuChoicesForEvent( choiceEventId );
-
-		}
-
-		this.eventTracker.addEvent( "nowait", eventToAdd );
-
-	};	
-
-
-
-
-
-
-
-
-
-
-
 	VN.prototype.sayLine = function sayLine( character, line, delay ) {
 
 		var self = this;
@@ -698,7 +548,6 @@
 			this.dialogButtonId = null;
 			this.dialogImageId = null;
 			this.dialogTextId = null;
-			this.dialogMenuId = null;
 
 			// dialog
 			this.dialogWidth = width;
@@ -739,10 +588,6 @@
 			// dialog character
 			this.dialogCharacter = null;
 
-			// choice
-			this.menuChoicesTaken = {};
-			this.menuChoices = {};
-
 			// timers
 			this.timers = {
 				"text": null
@@ -765,10 +610,6 @@
 
 		this.novelModeId = doc.getElementById( novelId + "-dialog-novelmode" );
 		this.dialogModeId = doc.getElementById( novelId + "-dialog-dialogmode" );
-		this.dialogMenuId = doc.getElementById( novelId + "-dialog-menu" );
-
-		// Init dialog menu container
-		this.hideDialogMenuContainer();
 
 		// Init novel mode container ( not implemented - hide )
 		this.hideNovelModeContainer();
@@ -782,12 +623,6 @@
 
 		// not yet implemented / tested
 		// this.setDialogBgImage( this.dialogBgImage, this.dialogBgImageWidth, this.dialogBgImageHeight );
-
-	};
-
-	Dialog.prototype.hideDialogMenuContainer = function hideDialogMenuContainer() {
-
-		this.dialogMenuId.style.display = "none";
 
 	};
 
@@ -1096,88 +931,6 @@
 
 		this.novelModeId.innerHTML = "";
 		this.dialogModeId.innerHTML = "";
-
-	};
-
-	Dialog.prototype.setMenuChoices = function setMenuChoices( template ) {
-
-		this.dialogMenuId.innerHTML = template;
-
-	};
-
-	Dialog.prototype.updateMenuChoicesReference = function updateMenuChoicesReference() {
-
-		this.dialogMenuChoiceContainerId = document.getElementById( this.novelId + "-dialogMenuChoiceContainer" );
-
-	};
-
-	Dialog.prototype.addMenuChoicesHandler = function addMenuChoicesHandler( choiceEventId, choices, callback ) {
-
-		var self = this;
-		var doc = document;
-		var novelId = this.novelId;
-		var onMenuChoiceClick = function( i ) {
-
-			var index = i;
-
-			return function clickDialogMenuChoiceButton() {
-
-				// hide menu
-				self.dialogMenuId.style.display = "none";
-
-				self.performMenuChoice( choiceEventId, index );
-
-				callback();
-
-			};
-
-		};
-
-		for ( var i = choices.length; i--; ) {
-
-			// TODO: add event delegation to menu choice container
-			doc.getElementById( novelId + "-dialogMenuChoiceButton" + i ).onclick = onMenuChoiceClick( i );
-
-		}
-
-	};
-
-	Dialog.prototype.performMenuChoice = function performMenuChoice( choiceEventId, indexInListOfChoices ) {
-
-		var choiceTaken = this.menuChoices[ choiceEventId ][ indexInListOfChoices ];
-
-		// store choice taken
-		this.menuChoicesTaken[ choiceEventId ] = choiceTaken;
-
-		var action = choiceTaken.action;
-
-		// register events in menu choice action
-		action();
-
-	};
-
-	Dialog.prototype.resetMenuChoicesForEvent = function resetMenuChoicesForEvent( choiceEventId ) {
-
-		this.menuChoices[ choiceEventId ] = [];
-
-	};
-
-	Dialog.prototype.resetMenuChoices = function resetMenuChoices() {
-
-		this.menuChoices = {};
-		this.menuChoicesTaken = {};
-
-	};
-
-	Dialog.prototype.setMenuChoicesDisplay = function setMenuChoicesDisplay( show ) {
-
-		this.dialogMenuId.style.display = show ? "block" : "none";
-
-	};
-
-	Dialog.prototype.setMenuChoicesPosition = function setMenuChoicesPosition( x, y ) {
-
-		this.dialogMenuChoiceContainerId.style.cssText += ";left:" + x + "px;top:" + y + "px;";
 
 	};
 
